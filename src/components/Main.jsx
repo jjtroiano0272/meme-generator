7c488a17c19f56e3d33fe9c88708a8905a5a09f7
jjtroiano0272@eagle.fgcu.edu <jjtroiano0272@eagle.fgcu.edu>
2021-12-13 15:59:09 -0500

    Fixes issues! Ready to deploy

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -42,28 +42,23 @@ export default function Main(props) {
   const [allMemes, setAllMemes] = useState([]);
 
   useEffect(() => {
-    // async function getMemes() {
-    //   const res = await fetch(apiEndpoint);
-    //   const data = await res.json();
-    //   setAllMemes(data.data.memes);
-    // }
-    fetch(apiEndpoint)
-      .then((res) => res.json())
-      .then((data) => setAllMemes(data.data.memes));
-  }, [allMemes]);
-
-  // this.setState({ randomImg: randomMemeImg })
+    async function getMemes() {
+      const res = await fetch(apiEndpoint);
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+    getMemes();
+  }, []);
 
   function getNewMeme() {
     const randNum = Math.floor(Math.random() * allMemes.length);
     const randMemeImg = allMemes[randNum].url;
-    console.log('getNewMeme called. URL: ', randMemeImg);
+
     // Keeps the previous text, gets new image
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: randMemeImg,
     }));
-    console.log('meme in state: ', meme);
   }
 
   function handleChange(event) {
@@ -74,68 +69,49 @@ export default function Main(props) {
     }));
   }
 
-  // const { topText, bottomText, randomImage } = meme;
+  const { topText, bottomText, randomImage } = meme;
 
   return (
     <div className='container'>
-      <form>
-        <div className='form-group mb-4 mt-2'>
-          <label className='text-muted' htmlFor='memeTopTextForm'>
-            Top text
-          </label>
-          {/* <input
-            type='text'
-            className='form-control search-query'
-            id='memeTopTextForm'
-            aria-describedby='Meme top text'
-            placeholder={`I'm a meme`}
-            name='topText'
-            value={meme.topText}
-            onChange={handleChange}
-          /> */}
-          <input
-            type='text'
-            placeholder="I'm a meme"
-            className='form-control search-query'
-            name='topText'
-            value={meme.topText}
-            onChange={handleChange}
-          />
-        </div>
-        <div className='form-group mb-2'>
-          <label className='text-muted' htmlFor='memeBottomTextForm'>
-            Bottom text
-          </label>
-          <input
-            type='text'
-            placeholder='So give me text, pls'
-            className='form-control search-query'
-            name='bottomText'
-            value={meme.bottomText}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button
-          className='btn btn-primary my-3 container-fluid'
-          onClick={getNewMeme}
-        >
-          Give me a new meme!
-        </button>
-        {/* {allMemes.map((item) => (
-          <img
-            className='img-fluid rounded my-3'
-            key={item.id}
-            src={randomImage}
-            alt=''
-          />
-        ))} */}
-        <div className='row'>
-          <img src={meme.randomImage} className='img-fluid rounded' />
-          <h2 className='caption top'>{meme.topText}</h2>
-          <h2 className='caption bottom'>{meme.bottomText}</h2>
-        </div>
-      </form>
+      <div className='form-group mb-4 mt-2'>
+        <label className='text-muted' htmlFor='memeTopTextForm'>
+          Top text
+        </label>
+        <input
+          type='text'
+          className='form-control search-query'
+          id='memeTopTextForm'
+          aria-describedby='Meme top text'
+          placeholder={`I'm a meme`}
+          name='topText'
+          value={topText}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='form-group mb-2'>
+        <label className='text-muted' htmlFor='memeBottomTextForm'>
+          Bottom text
+        </label>
+        <input
+          type='text'
+          placeholder='So give me text, pls'
+          className='form-control search-query'
+          name='bottomText'
+          value={bottomText}
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        className='btn btn-primary my-3 container-fluid'
+        onClick={getNewMeme}
+      >
+        Give me a new meme!
+      </button>
+      <div className='row position-relative'>
+        <img src={randomImage} className='img-fluid rounded mw-100' />
+        <h2 className='caption top'>{topText}</h2>
+        <h2 className='caption bottom'>{bottomText}</h2>
+      </div>
     </div>
   );
 }
